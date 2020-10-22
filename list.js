eed11dd485e83a4b605a4196a2b6a1370136b9cb
Tiago <tbarros1337@hotmail.com>
2020-10-21 21:16:47 -0300

    structuring the app

---
+++ list.js
@@ -0,0 +1,6 @@
+const listsElement = document.querySelector("#app ul");
+const inputElement = document.querySelector("#app input");
+const buttonElement = document.querySelector("#app button");
+
+let lists = ["Pão de hamburger", "Mel", "Cobertura de chocolate"];
+
