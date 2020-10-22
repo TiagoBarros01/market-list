9c9ae1db3b623369eac21d7215ecb10ec54c51d4
Tiago <tbarros1337@hotmail.com>
2020-10-21 21:19:28 -0300

    render list

--- list.js
+++ list.js
@@ -1,6 +1,17 @@
-const listsElement = document.querySelector("#app ul");
-const inputElement = document.querySelector("#app input");
-const buttonElement = document.querySelector("#app button");
+let listsElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
 
 let lists = ["Pão de hamburger", "Mel", "Cobertura de chocolate"];
 
+function renderLists() {
+    for (list of lists) {
+        const listElement = document.createElement("li");
+        const listText = document.createTextNode(list)
+
+        listElement.appendChild(listText)
+        listsElement.appendChild(listElement)
+    }
+}
+
+renderLists()
