95bf96f6b6ac616f2a7e22a70e429f841bf25b2c
Tiago <tbarros1337@hotmail.com>
2020-11-07 14:16:08 -0300

    updating

--- list.js
+++ list.js
@@ -1,13 +1,16 @@
 let title = document.querySelector(".title-header");
+let app = document.querySelector(".position-item");
 let listsElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
 let lists = JSON.parse(localStorage.getItem("listItems")) || [];
 
-if (lists.length > 0) {
+if (lists.length >= 1) {
   title.style.display = "none";
-} 
+}
+
+console.log(lists.length);
 
 function renderLists() {
   listsElement.innerHTML = "";
@@ -39,23 +42,37 @@ function renderLists() {
 
 renderLists();
 
+let total = 0;
+let maxItemsImg = document.createElement("img");
+maxItemsImg.setAttribute("src", "img/warning.svg");
+maxItemsImg.setAttribute("class", "warning");
+
+let maxItems = document.createElement("div");
+let maxItemsText = document.createTextNode("Número máximo atingido!");
+maxItems.setAttribute("class", "max-items");
+
 function addItem() {
   let listText = inputElement.value;
 
-  if (lists.length >= 12) {
-    alert(
-      "Você atingiu o número máximo de itens na lista!\nDelete alguns itens para adicionar outros"
-    );
-  } 
-  else if (listText === "") {
-    console.warn("Escreva algo para adicionar a lista!");
-    alert("Escreva algo para adicionar a lista!");
-  } 
-  else {
-    lists.push(listText);
+  if (lists.length >= 0 && listText !== "") {
+    title.style.display = "none";
   }
 
-  title.style.display = "none";
+  if (lists.length >= 12 && total < 1) {
+    total++;
+    maxItems.appendChild(maxItemsImg);
+    maxItems.appendChild(maxItemsText);
+    app.appendChild(maxItems);
+    app.style.display = "flex";
+    app.style.transition = "1s";
+
+    console.log(total);
+  } else if (listText === "") {
+    console.warn("Escreva algo para adicionar a lista!");
+    alert("Escreva algo para adicionar a lista!");
+  } else if (lists.length < 12) {
+    lists.push(listText);
+  }
 
   inputElement.value = "";
   renderLists();
@@ -65,6 +82,9 @@ function addItem() {
 buttonElement.onclick = addItem;
 
 function deleteItem(pos) {
+  app.style.display = "none";
+  total = 0;
+  console.log(total);
   lists.splice(pos, 1);
   renderLists();
   saveToStorage();
